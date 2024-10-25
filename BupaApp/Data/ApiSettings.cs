4efdb8ded83681f6216a07b115c48cfc0cba0606
Jamesy5567 <jamesgriffith724@gmail.com>
2024-10-25 14:52:04 +0100

    Added Api Settings

---
+++ BupaApp/Data/ApiSettings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BupaApp.Data
+{
+    public class ApiSettings
+    {
+        public string BaseUrl {get; set;}
+        public string ApiKey {get; set;}
+    }
+}
