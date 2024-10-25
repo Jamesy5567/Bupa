990f86b40a2b05e8c468000faaeeb4aceb44e893
Jamesy5567 <jamesgriffith724@gmail.com>
2024-10-25 16:19:31 +0100

    Added Get Method

--- BupaApp/Interfaces/IVehicleRepository.cs
+++ BupaApp/Interfaces/IVehicleRepository.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BupaApp.Models;
 
 namespace BupaApp.Interfaces
 {
     public interface IVehicleRepository
     {
-        
+        Task<Vehicle> GetVehicle(string plate);
     }
 }
--- BupaApp/Repositories/VehicleRepository.cs
+++ BupaApp/Repositories/VehicleRepository.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BupaApp.Interfaces;
+using BupaApp.Data;
+using Microsoft.Extensions.Options;
+using BupaApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace BupaApp.Repositories
 {
     public class VehicleRepository : IVehicleRepository
     {
-        
+        private readonly HttpClient _httpClient;
+        private readonly ApiSettings _apiSettings;
+
+        public VehicleRepository(HttpClient httpClient, IOptions<ApiSettings> apiSettings)
+        {
+            _httpClient = httpClient;
+            _apiSettings = apiSettings.Value;
+
+            _httpClient.BaseAddress = new Uri(_apiSettings.BaseUrl);
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_apiSettings.ApiKey}");
+        }
+
+        [HttpGet]
+        public async Task<Vehicle>GetVehicle(string registrationplate)
+        {
+            var response = await _httpClient.GetAsync($"{registrationplate}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<Vehicle>(jsonResponse);
+            }
+            else 
+            {
+                throw new HttpRequestException($"Failed to fetch vehicle details: {response.ReasonPhrase}");
+            }
+        }
     }
 }
